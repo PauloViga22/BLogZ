97737cffa451b19cb9b2768b6a94f37bab2bd2d0
PAULO VICTOR DE CASTRO VIGA <paulo.22.5@gmail.com>
2020-02-17 22:22:25 -0800

    fixing the Like btn logic

--- routes/route.js
+++ routes/route.js
@@ -9,6 +9,7 @@ router.post('/posts', controller.addPost);
 router.put('/posts/:id/update', controller.updatePost);
 
 router.put('/posts/like/:id', controller.liker);
+router.put('/posts/like/:id/:like', controller.liker);
 
 router.get('/posts/:id', controller.getPostDetailPage);
 router.delete('/posts', controller.deleteAllPosts);
